cdd47408eea66df388e8252193176df0282bd8f6
azikrasalma <azikrasalma352@gmail.com>
2022-10-10 21:21:44 +0700

    menyelesaikan video 6

---
+++ phpmvc/app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_A_203040016/phpmvc/public');
--- phpmvc/app/views/home/index.php
+++ phpmvc/app/views/home/index.php
@@ -1,2 +1,11 @@
-
-    <h1>Selamat Datang di Website Saya</h1>
+<div class="container">
+  <div class="jumbotron mt-3">
+    <h1 class="display-4">Selamat Datang di Website Saya!</h1>
+    <p class="lead">Buat kamu yang disana</p>
+    <hr class="my-3">
+    <p>Jaga kesehatan ya!!! dan jangan lupa makan oke :)
+    </p>
+    <a class="btn btn-primary btn-lg" href="" role="button">Sehat dan Bahagia Selalu!</a>
+  </div>
+  </div>
+</div>
